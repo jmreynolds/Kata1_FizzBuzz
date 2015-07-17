afb0461a1d4f0df08aa1f07fe6e71f80ba78f8a3
Joseph Reynolds <jreynolds@infocraft.net>
2015-07-16 23:54:44 -0500

    Solution is set up Failing test is ready

---
+++ src/FizzBuzzLibrary/FizzBuzz.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzLibrary
+{
+    public class FizzBuzz
+    {
+        public string Translate(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/FizzBuzzTests/TranslateTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FizzBuzzLibrary;
+using NUnit.Framework;
+using Should;
+
+namespace FizzBuzzTests
+{
+    
+    public class TranslateTests
+    {
+        [Test]
+        public void Should_Translate_1_as_1()
+        {
+            var fizzbuzz = new FizzBuzz();
+            fizzbuzz.Translate("1").ShouldEqual("1");
+        }
+    }
+}
